2a867abfc3db7d68da8d0cabe20a7653e72ca02a
温不胜 <519710938@qq.com>
2023-09-04 00:03:04 +0800

    ✨ feat(components): input-number组件容错逻辑梳理

--- packages/components/input-number/src/input-number.vue
+++ packages/components/input-number/src/input-number.vue
@@ -8,7 +8,7 @@
       :class="[
         bem.e('controls'),
         bem.e('decrease'),
-        bem.is('disabled', disabled)
+        bem.is('disabled', minDisabled)
       ]"
       v-if="controls"
     >
@@ -20,7 +20,7 @@
       :class="[
         bem.e('controls'),
         bem.e('increase'),
-        bem.is('disabled', disabled)
+        bem.is('disabled', maxDisabled)
       ]"
       v-if="controls"
     >
@@ -38,6 +38,9 @@
       :min="min"
       :name="name"
       :label="label"
+      @input="handleInput"
+      @blur="handleBlur"
+      @focus="handleFocus"
     />
   </div>
 </template>
@@ -49,9 +52,85 @@ import SIcon from '@storm/components/icon'
 import SInput from '@storm/components/input'
 import Decrease from '@storm/components/internal-icon/decrease';
 import Increase from '@storm/components/internal-icon/increase';
+import { computed, onMounted, watch, reactive, ref } from 'vue'
+import { isNumber, isNil } from '@storm/utils'
+import { UPDATE_MODEL_EVENT } from '@storm/constants';
+import { isUndefined } from '@storm/utils';
 defineOptions({ name: 'SInputNumber' })
-defineProps(inputNumberProps)
-defineEmits(inputNumberEmits)
+const props = defineProps(inputNumberProps)
+const emit = defineEmits(inputNumberEmits)
+
+interface Data {
+  currentValue: number | null | undefined,
+  userInput: null | number | string
+}
 
 const bem = createNamespace('input-number')
+const inputRef = ref<HTMLInputElement>()
+const data = reactive<Data>({
+  currentValue: props.modelValue,
+  userInput: null
+})
+
+const minDisabled = computed(() => isNumber(props.modelValue) && props.modelValue <= props.min)
+const maxDisabled = computed(() => isNumber(props.modelValue) && props.modelValue >= props.max)
+
+const verifyValue = (value: number | null | undefined, update?: boolean): number | null | undefined => {
+  const { min, max, step, precision } = props
+  if (max < min) {
+    throw new Error('[InputNumber] min should not be greater than max.')
+  }
+  let newVal = Number(value)
+  if (isNil(value) || isNaN(newVal)) {
+    return
+  }
+  // if (!isUndefined(precision)) {
+  //   newVal = toPrecision(newVal, precision)
+  // }
+
+  // 如果超出界限将值限制在min和max之间
+  if (newVal > max || newVal < min) {
+    newVal = newVal > max ? max : min
+    update && emit(UPDATE_MODEL_EVENT, newVal)
+  }
+}
+
+const setCurrentValue = (value: number | string | null | undefined, emitChange = true) => {
+  const oldVal = data.currentValue
+  const newVal = verifyValue(value)
+}
+const handleInput = (value: string) => {
+  data.userInput = value
+  const newVal = value === '' ? null : Number(value)
+  setCurrentValue(newVal, false)
+}
+const handleFocus = (event: MouseEvent | FocusEvent) => {
+  emit('focus', event)
+}
+const handleBlur = (event: MouseEvent | FocusEvent) => {
+  emit('blur', event)
+}
+
+onMounted(() => {
+  // 如果modelValue绑定的不是null和undefined 就强转为number类型
+  // 如果转出来不是一个数字就赋值为undefined
+  if (!isNumber(props.modelValue) && props.modelValue != null) {
+    let val: number | undefined = Number(props.modelValue)
+    if (isNaN(val)) {
+      val = undefined
+    }
+    emit(UPDATE_MODEL_EVENT, val)
+  }
+})
+// 暴露出去给外部调用
+const focus = () => {
+  inputRef.value?.focus?.()
+}
+const blur = () => {
+  inputRef.value?.blur?.()
+}
+defineExpose({
+  focus,
+  blur
+})
 </script>
--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -4,5 +4,6 @@ export { isArray, isString } from '@vue/shared'
 export const isUndefined = (val: any): val is undefined => val === undefined
 export const isBoolean = (val: any): val is boolean => typeof val === 'boolean'
 export const isNumber = (val: any): val is number => typeof val === 'number'
+export const isNil = (val: any): val is undefined | null => val == null
 
 export const iconPropType = [String, Object, Function] as PropType<string | Component>

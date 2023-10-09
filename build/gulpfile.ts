3c779382b3fb2f72baf9dd2dd37c230b6cf6ae0e
温不胜 <519710938@qq.com>
2023-10-09 23:39:55 +0800

    🔧 build(theme-chalk): 打包theme-chalk流程

--- build/gulpfile.ts
+++ build/gulpfile.ts
@@ -1,6 +1,11 @@
-import { series } from 'gulp'
+import { series, parallel } from 'gulp'
 import { run, withTaskName } from './utils'
 
 export default series(
-  withTaskName('clean', () => run('rm -rf dist'))
+  // 删除dist目录
+  withTaskName('clean', () => run('rm -rf dist')),
+  parallel(
+    // 执行packages/theme-chalk目录下的build命令打包样式
+    withTaskName('buildThemeChalk', () => run('pnpm run -C packages/theme-chalk build'))
+  )
 )
--- build/utils/paths.ts
+++ build/utils/paths.ts
@@ -1,3 +1,7 @@
-import path from 'path'
-
-export const projectRoot = path.resolve(__dirname, '..', '..')
+import { resolve } from 'path'
+// 根路径
+export const projectRoot = resolve(__dirname, '..', '..')
+// dist
+export const buildOutput = resolve(projectRoot, 'dist')
+// /dist/storm-ui
+export const suOutput = resolve(buildOutput, 'storm-ui')

7c06d2fb79eaf29dc922034090e52d7400f2f741
温不胜 <519710938@qq.com>
2023-10-16 22:17:27 +0800

    ✨ feat(build): 打包测试

--- build/tasks/modules.ts
+++ build/tasks/modules.ts
@@ -2,14 +2,14 @@ import glob from 'fast-glob'
 import { rollup } from 'rollup'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import vue from 'rollup-plugin-vue'
+import ts from 'rollup-plugin-typescript2'
 import commonjs from '@rollup/plugin-commonjs'
-import esbuild from 'rollup-plugin-esbuild'
 import { pkgRoot } from '../utils/paths'
 import { excludeFiles } from '../utils'
 
 export const buildModules = async () => {
   const input = excludeFiles(
-    await glob('**/*.{js,ts,vue}', {
+    await glob('**/*.{ts,vue}', {
       cwd: pkgRoot,
       absolute: true, // 返回绝对路径
       onlyFiles: true,
@@ -18,17 +18,12 @@ export const buildModules = async () => {
   const bundle = await rollup({
     input,
     plugins: [
-      vue(),
       nodeResolve(),
+      vue(),
+      ts(),
       commonjs(),
-      esbuild({
-        sourceMap: true,
-        target: 'es2018',
-        loaders: {
-          '.vue': 'ts',
-        },
-      }),
     ],
-    treeshake: false
+    treeshake: false,
+    external: ['vue']
   })
 }

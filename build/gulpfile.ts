e891a2ce4e2b8f4814cb6c72f4ddfac6a3fb9c53
温不胜 <519710938@qq.com>
2023-10-21 00:35:55 +0800

    🔧 build(build): buildThemeChalk流程

--- build/gulpfile.ts
+++ build/gulpfile.ts
@@ -1,6 +1,15 @@
 import { series, parallel } from 'gulp'
 import { run, runTask, suOutput, withTaskName } from './src'
-import { mkdir } from 'fs/promises'
+import { mkdir, copyFile } from 'fs/promises'
+import path from 'path'
+
+const copyFullStyle = async () => {
+  await mkdir(path.resolve(suOutput, 'dist'), { recursive: true })
+  await copyFile(
+    path.resolve(suOutput, 'theme-chalk/index.css'),
+    path.resolve(suOutput, 'dist/index.css')
+  )
+}
 
 export default series(
   // 删除打包目录
@@ -9,7 +18,13 @@ export default series(
   withTaskName('createOutput', () => mkdir(suOutput, { recursive: true })),
   parallel(
     runTask('buildModules'),
-    runTask('buildFullBundle')
+    runTask('buildFullBundle'),
+    series(
+      withTaskName('buildThemeChalk', () =>
+        run('pnpm run -C packages/theme-chalk build')
+      ),
+      copyFullStyle
+    )
   )
 )
 
